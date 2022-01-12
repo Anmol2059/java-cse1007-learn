07715dcb3b4d92e43e3fdc827320969a87d6e821
Anmol Guragain <officialanmol98@gmail.com>
2022-01-12 09:04:40 +0545

    day 2

---
+++ practice works/Inout.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+
+public class Inout {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter name: ");
+        String name = sc.nextLine();
+        System.out.println("Welcome" + name);
+        
+
+    }
+    
+    
+}
